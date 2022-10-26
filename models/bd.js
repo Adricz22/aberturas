db5fea518d1e3cae3965292f7c1cf79fd66c382c
Adrián Chávez <adri-142@hotmail.com>
2022-10-26 10:27:05 -0300

    rows

--- models/bd.js
+++ models/bd.js
@@ -2,18 +2,14 @@ var mysql = require("mysql2");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "containers-us-west-106.railway.app",
-  user: "root",
-  password: "H1jpKQvDZDme5yXwKDCM",
-  database: "railway",
-  port: 7029,
-  // host: process.env.MYSQL_HOST,
-  // user: process.env.MYSQL_USER,
-  // password: process.env.MYSQL_PASSWORD,
-  // database: process.env.MYSQL_DB_NAME,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  port: process.env.MYSQL_PORT,
 });
 
-let sql = "SELECT * FROM novedades";
+let sql = "SELECT * FROM usuarios";
 
 pool.execute(sql, (err, result) => {
   if (err) throw err;
