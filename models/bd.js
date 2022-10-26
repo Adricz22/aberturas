b4a7be9ca09316024f84a62f2c7ffa3f55e702e3
Adrián Chávez <adri-142@hotmail.com>
2022-10-26 09:32:39 -0300

    mysql

--- models/bd.js
+++ models/bd.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 var mysql = require("mysql2");
 
 var pool = mysql.createPool({
@@ -8,13 +8,6 @@ var pool = mysql.createPool({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB_NAME,
 });
-
-MYSQL_HOST=containers-us-west-106.railway.app
-MYSQL_USER=root
-MYSQL_PASSWORD=H1jpKQvDZDme5yXwKDCM
-MYSQL_DB_NAME=railway
-MYSQL_PORT=7029
-MYSQL_URL=mysql://${{ MYSQL_USER }}:${{ MYSQL_PASSWORD }}@${{ MYSQL_HOST }}:${{ MYSQL_PORT }}/${{ MYSQL_DB_NAME }}
 
 let sql = "SELECT * FROM usuarios";
 
