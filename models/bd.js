d96ffcf26a1dc8d1aa927558bee76aed39bc64d2
Adrián Chávez <adri-142@hotmail.com>
2022-10-27 09:31:18 -0300

    update variables

--- models/bd.js
+++ models/bd.js
@@ -2,18 +2,24 @@ var mysql = require("mysql2");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
-  port: process.env.MYSQL_PORT,
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_DB_NAME,
+  database: process.env.RDS_DB_NAME,
 });
 
 // test connection
 let sql = "SELECT * FROM usuarios";
 pool.execute(sql, (err, result) => {
-  if (err) throw err;
-  console.log("RESULT ", result);
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to database.");
+
+  // if (err) throw err;
+  // console.log("RESULT ", result);
 });
 
 module.exports = pool.promise();
