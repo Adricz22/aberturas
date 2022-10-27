4ecb01d5ca759427e62817631cdaaeac08700818
Adrián Chávez <adri-142@hotmail.com>
2022-10-27 10:59:37 -0300

    mysql2

--- models/bd.js
+++ models/bd.js
@@ -2,11 +2,16 @@ var mysql = require("mysql2");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_DB_NAME,
-  database: process.env.RDS_DB_NAME,
+  host: "db-instance-demo.crpbkchr0sv6.sa-east-1.rds.amazonaws.com",
+  user: "root",
+  password: "YoERh59O5NkKtfAR",
+  port: 3306,
+  database: "database_aws",
+  // host: process.env.RDS_HOSTNAME,
+  // user: process.env.RDS_USERNAME,
+  // password: process.env.RDS_PASSWORD,
+  // port: process.env.RDS_DB_NAME,
+  // database: process.env.RDS_DB_NAME,
 });
 
 // test connection
