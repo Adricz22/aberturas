37c4d5efccc66f26d720b572ceffef8ec2c82146
Adrián Chávez <adri-142@hotmail.com>
2023-08-13 11:32:37 -0300

    clear session sig

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -12,7 +12,7 @@ router.get("/", function (req, res, next) {
 router.get("/logout", function (req, res, next) {
   // req.session.destroy();
   // req.session = null;
-  res.clearCookie("session");
+  res.clearCookie("session", "session.sig");
   res.render("admin/login", {
     layout: "admin/layout",
   });
