3d4ae5f1969786a27811ddaf13dd337873ab5db2
Adrián Chávez <adri-142@hotmail.com>
2023-08-14 17:01:41 -0300

    favicon ico

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(favicon(path.join(__dirname, "public/favicon.ico")));
+app.use(favicon(path.join(__dirname, "public", "/favicon.ico")));
 
 // Returns a middleware to serve favicon
 // app.use(favicon(__dirname + '/favicon.ico'));
